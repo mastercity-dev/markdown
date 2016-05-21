3fa4f484a6f06424c7b1daa3993b9157b3502172
Bestreyer <sysolyatindima@Sysolyatins-MacBook-Air.local>
2016-05-21 23:18:39 +0300

    add Twig extension

---
+++ src/Twig/Extension.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace Mastercity\Markdown\Twig;
+
+
+use Mastercity\Markdown\Parser\Parser;
+
+class Extension extends \Twig_Extension
+{
+
+    protected $parser;
+
+    public function __construct(Parser $parser)
+    {
+        $this->parser = $parser;
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return "mastercity.markdown";
+    }
+
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('markdown', [$this, 'markdownFilter'])
+        ];
+    }
+
+    public function markdownFilter($text)
+    {
+        return $this->parser->text($text);
+    }
+}
