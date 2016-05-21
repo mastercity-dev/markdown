cc239051550a8932eaf79897acabea83831640cd
Bestreyer <sysolyatindima@Sysolyatins-MacBook-Air.local>
2016-05-21 23:30:46 +0300

    fix error class name

--- src/MarkdownBundle/DependencyInjection/MarkdownExtension.php
+++ src/MarkdownBundle/DependencyInjection/MarkdownExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class AppExtension extends Extension
+class MarkdownExtension extends Extension
 {
 
     /**
